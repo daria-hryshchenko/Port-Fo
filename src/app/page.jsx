b64da7d3af4f991705baad7d740bfc2239eb73de
Daria Hryshchenko <daria199210@gmail.com>
2024-06-30 16:57:16 +0300

    add navbar

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,16 @@
+import Image from "next/image";
+
 const Homepage = () => {
-  return <div></div>;
+  return (
+    <div className="flex flex-col">
+    {/* IMAGE CONTAINER */}
+    <div className="h-1/2">
+      {/* <Image src="/hero.png"/> */}
+    </div>
+    {/* TEXT CONTAINER */}
+    <div className="h-1/2"></div>
+  </div>
+  );
 };
 
 export default Homepage;
